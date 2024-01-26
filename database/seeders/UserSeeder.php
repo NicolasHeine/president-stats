c5efe0827e226430f529d67118946b83e1c6ac81
NicolasHeine <nicolas.heine11@gmail.com>
2024-01-26 14:08:50 +0100

    feat(admin): add admin seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('adminadmin'),
+            'is_admin' => 1
+        ]);
+    }
+}

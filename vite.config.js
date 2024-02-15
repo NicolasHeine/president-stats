1003771942dfba9bc48d95476a17c0604d810126
NicolasHeine <nicolas.heine11@gmail.com>
2024-02-15 17:10:38 +0100

    fix(vite): fix config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 'resources/scss/style.scss',
                 'resources/scss/admin.scss',
-                'resources/js/app.js'
+                'resources/js/app.js',
+                'resources/js/admin.js'
             ],
             refresh: true,
         }),
